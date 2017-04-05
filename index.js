6b4da2f84f0e4491c30d2574b6aae396204a9456
josemsantos <jose.santos@jumia.com>
2017-04-06 00:39:46 +0100

    Export the generate changelog as a module

--- index.js
+++ index.js
@@ -5,47 +5,52 @@ var Datahandler = require('./data-handler');
 var Client = require('./github-api-requests');
 var Writer = require('./write-changelog');
 
-var client = new Client();
-client.setGithubToken(argv.token || argv.t);
-client.setOwnerRepo(argv.repo || argv.r);
+function generateChangeLog() {
 
-var dhandler = new Datahandler();
+    var client = new Client();
+    client.setGithubToken(argv.token || argv.t);
+    client.setOwnerRepo(argv.repo || argv.r);
 
-var writer = new Writer(argv.repo || argv.r);
-writer.setBaseLog(argv.baselog || argv.b);
+    var dhandler = new Datahandler();
+
+    var writer = new Writer(argv.repo || argv.r);
+    writer.setBaseLog(argv.baselog || argv.b);
 
 
-client.getMilestones().then(function(response){
-    if (response.body.length) {
-        Promise.all(
-            response.body.map(function(milestone){
-                return getPrData(milestone);
-            })
-        ).then(values => {
-            dhandler.sortReleasesIssuesByTypePosition();
-            writer.setData(dhandler.getReleases());
-            writer.writeChangeLog();
-        }).catch(reason => { 
-            console.log(reason)
+    client.getMilestones().then(function(response){
+        if (response.body.length) {
+            Promise.all(
+                response.body.map(function(milestone){
+                    return getPrData(milestone);
+                })
+            ).then(values => {
+                dhandler.sortReleasesIssuesByTypePosition();
+                writer.setData(dhandler.getReleases());
+                writer.writeChangeLog();
+            }).catch(reason => { 
+                console.log(reason)
+            });
+        }
+    }).catch(function(err){
+        console.log(err);
+    });
+
+    function getPrData(milestone) {
+        return new Promise(function(resolve, reject) {
+            client.getPRInMilestone(milestone.title).then(function(response) {
+                if (response.items.length) {
+                    dhandler.addRelease(dhandler.toMileStone(milestone));
+                    response.items.forEach(function(pr, i) {
+                        dhandler.addIssue(dhandler.toIssue(pr));
+                    });
+                }
+                resolve();
+            }).catch(function(err) {
+                console.log(err);
+                rejected(err);
+            });
         });
     }
-}).catch(function(err){
-    console.log(err);
-});
-
-function getPrData(milestone) {
-    return new Promise(function(resolve, reject) {
-        client.getPRInMilestone(milestone.title).then(function(response) {
-            if (response.items.length) {
-                dhandler.addRelease(dhandler.toMileStone(milestone));
-                response.items.forEach(function(pr, i) {
-                    dhandler.addIssue(dhandler.toIssue(pr));
-                });
-            }
-            resolve();
-        }).catch(function(err) {
-            console.log(err);
-            rejected(err);
-        });
-    });
 }
+
+module.exports = generateChangeLog;
